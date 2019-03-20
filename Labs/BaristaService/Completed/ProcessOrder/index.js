12614ab99adba93018b18e141ca68292ce1f7232
Vadim Kacherov <vkacherov@gmail.com>
2019-03-20 12:54:56 -0400

    Init check in for Barista Service

---
+++ Labs/BaristaService/Completed/ProcessOrder/index.js
@@ -0,0 +1,12 @@
+module.exports = function(context, pendingQueueMsg) {
+    context.log('JavaScript ServiceBus queue trigger function processed message', pendingQueueMsg);
+
+    //
+    // The service logic is here 
+    // For now just set the staus to completed and drop it in the completed queue
+    //
+    pendingQueueMsg.status = "COMPLETED";
+    context.log('Order completed, writing to completed orders queue: '+ JSON.stringify(pendingQueueMsg));
+    context.bindings.completedQueue = pendingQueueMsg;
+    context.done();
+};
