db6867f7b02992708c4668127b1840bd75d48412
Vadim Kacherov <vkacherov@gmail.com>
2019-03-20 14:28:24 -0400

    Adding completed service src

--- Labs/BaristaService/Completed/ProcessOrder/index.js
+++ Labs/BaristaService/Completed/ProcessOrder/index.js
@@ -5,8 +5,10 @@ module.exports = function(context, pendingQueueMsg) {
     // The service logic is here 
     // For now just set the staus to completed and drop it in the completed queue
     //
-    pendingQueueMsg.status = "COMPLETED";
-    context.log('Order completed, writing to completed orders queue: '+ JSON.stringify(pendingQueueMsg));
-    context.bindings.completedQueue = pendingQueueMsg;
+    if( pendingQueueMsg ) {
+        pendingQueueMsg.status = "COMPLETED";
+        context.log('Order completed, writing to completed orders queue: '+ JSON.stringify(pendingQueueMsg));
+        context.bindings.completedQueue = pendingQueueMsg;
+    }
     context.done();
 };
