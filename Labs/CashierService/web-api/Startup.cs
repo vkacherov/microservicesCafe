6fb80adb51bf76b7376f53286b4a63f1ba88a5de
Timothy Baggs <tbaggs@microsoft.com>
2019-03-25 17:57:56 -0400

    Updated startup

--- Labs/CashierService/web-api/Startup.cs
+++ Labs/CashierService/web-api/Startup.cs
@@ -48,16 +48,6 @@ namespace CashierServices
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
@@ -67,6 +57,17 @@ namespace CashierServices
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cashier Services V1");
             });
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
             app.UseMvc();
