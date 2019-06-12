dae9cbb20301b3c99d960c8d2801fc7d66a4d0d8
Timothy Baggtbstbaggs <tbaggs@mitbaggscrosoft.comttttba>
2019-06-12 15:46:09 -0400

    Updated queue services to send over firewall friendly amqpwebsockets

--- Labs/CashierService/web-api/Services/MessageQueueService.cs
+++ Labs/CashierService/web-api/Services/MessageQueueService.cs
@@ -1,5 +1,7 @@
 using CashierServices.Contracts;
 using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Microsoft.Azure.ServiceBus.Primitives;
 using Microsoft.Extensions.Options;
 using System.Text;
 
@@ -11,10 +13,16 @@ namespace CashierServices.Services
         
         public MessageQueueService (IOptions<ApplicationSettings> settings)
         {
+            var builder = new ServiceBusConnectionStringBuilder(settings.Value.ServiceBusConnection);
+
+            var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(
+                builder.SasKeyName,
+                builder.SasKey);
+
             _queueClient = 
                 new QueueClient(
-                    settings.Value.ServiceBusConnection,
-                    settings.Value.QueueName);
+                    builder.Endpoint,
+                    settings.Value.QueueName, tokenProvider, TransportType.AmqpWebSockets);
         }
         public void SendMessage(string  message)
         {
